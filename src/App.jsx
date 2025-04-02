a970979f74a8f94c090048c4d056ae64ca549111
HadriaMartins <rm93892@estudante.fieb.edu.br>
2025-04-02 08:14:56 -0300

    passagem de props

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from "react"
+import {useState} from 'react'
 
 function App(){
 
   const [nome, setNome] = useState('Ronaldo');
 
-  const Formulario = () => {
+  const Formulario = (props) => {
     return (
       <>
-      <h4>Formulário de Inscrição</h4>
-      <input className="Nome"
-        name="nome"
-        placeholder="Digite seu nome..."
-        onChange={(e)=>setNome(e.target.value)}
-        type="text" />
-      <button onClick={()=>{alert(nome)}}>
-        CLIQUE AQUI
-      </button>
+        <h4>Formulário de Inscrição</h4>
+        <input className="Nome"
+            name = "nome"
+            placeholder="Digite seu nome..."
+            onChange={(e)=>props.nome = e.target.value}
+            type="text" />
+        <button onClick={()=>{
+          (props.texto !=null) ? alert(props.texto) : alert(props.nome)
+          }}>
+          CLIQUE AQUI
+        </button>
       </>
     )
   }
+
   return (
     <div>
-      <h3>Pizarria 2A</h3>
-      <Formularios/>
-      <Formularios/>
-      <Formularios/>
+      <h3>Pizzaria 2A</h3>
+      <Formulario texto="texto enviado pelo app"/>
+      <Formulario texto="Santos meu amor"/>
+      <Formulario texto="Novo texto enviado pelo app"/>
+      <Formulario nome={nome}/>
 
     </div>
   )
 }
 
 export default App
+ 
